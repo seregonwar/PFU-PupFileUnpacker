af46d9c59ebab723b04a62239c2ab2fa49760fcb
Seregon <109359355+seregonwar@users.noreply.github.com>
2023-08-16 23:06:20 +0200

    other problems solved
    The majority of issues pertaining to the pup_unpacker.py have been successfully addressed. However, there persists a lingering concern concerning the proper recognition of the MAGIC attribute within PUP files, thus rendering them invalid. My conjecture is that the root of this quandary resides within the code of the ps4_dec_pup_info.py file, which has not been brought up to date with the most recent PS4 firmware iteration. Should this script undergo a comprehensive update, it could potentially facilitate the extraction of the latest PUP files for both the PS4 and PS5 platforms.

--- pup_unpacker.py
+++ pup_unpacker.py
@@ -186,6 +186,9 @@ def __init__(self, file_path):
         # Estrae il nome del file senza l'estensione
         pup_name = os.path.splitext(os.path.basename(file_path))[0]
 # Definisci la funzione per aprire la finestra di dialogo e selezionare il file .pup
+class Pup:
+  MAGIC = b'\x01\x4F\xC1\x0A' 
+
 def select_file():
     root = tk.Tk()
     root.withdraw()
@@ -317,63 +320,40 @@ class PupUnpacker:
         else:
             self.extract_button.configure(state=DISABLED)
 
-    def extract_pup(self):
-        # Ottiene il percorso del file selezionato
-        file_path = self.file_label.cget('text')
+   # Import ed eventuali definizioni
 
-        if not os.path.exists(file_path):
-            messagebox.showerror("Errore", f"Il file {file_path} non esiste.")
-            return
+# Classe PupUnpacker
+class PupUnpacker:
 
-        # Crea una directory temporanea per l'estrazione dei file
-        with tempfile.TemporaryDirectory() as temp_dir:
-            # Estrae i file dal file .pup nella directory temporanea
-            try:
-                if PS4_DEC_PUP_INFO_INSTALLED:
-                    ps4_dec_pup_info.extract_pup(file_path, temp_dir)
-                else:
-                    from pup_extractor import extract_pup
-                    extract_pup(file_path, temp_dir)
-            except Exception as e:
-                messagebox.showerror("Errore", f"Errore durante l'estrazione del file {file_path}: {str(e)}")
-                return
+  def __init__(self):
+    # codice per inizializzare oggetto e GUI
 
+   def extract_pup(self):
+    
+    file_path = ... 
+    # codice esistente fino ad apertura file
 
-        # Crea una directory con lo stesso nome del file .pup nella stessa cartella
-dir_path = os.path.dirname(file_path)
-pup_name = os.path.splitext(os.path.basename(file_path))[0]
+    with open(file_path, 'rb') as f:
+      buffer = f.read()
 
-with open(file_path, 'rb') as f:
-    header = f.read(HEADER_SIZE)
-    if not header.startswith(MAGIC):
-        raise ValueError(f"Il file {pup_name} non e un PUP valido.")
-    version = header[4:8]
-    if version != VERSION:
-        raise ValueError(f"La versione del file {pup_name} non e supportata.")
-    mode = header[8:12]
-    if mode not in MODES:
-        raise ValueError(f"Il modo del file {pup_name} non e supportato.")
-    size = int.from_bytes(header[16:20], byteorder='little')
-    sha1_hash = header[28:48]
-    padding = header[HEADER_SIZE-PADDING_SIZE:]
-    if padding != PADDING:
-        raise ValueError(f"Il file {pup_name} non ha il padding corretto.")
+    # Spostare questo codice in metodi di classe
+    self.validate_header(buffer)  
+    self.extract_entries(buffer)
 
-    # Definizione della classe Pup
-    # Costruisce la lista dei file nel PUP
-    self.entry_table = []
-    for i in range(num_entries):
-        entry_offset = entry_table_offset + (i * entry_size)
-        entry = buffer[entry_offset:entry_offset + entry_size]
-        entry_info = struct.unpack("<QII", entry)
-        entry_name_offset = entry_info[0] - data_offset
-        entry_name_end_offset = buffer.find(b'\x00', entry_name_offset)
-        entry_name = buffer[entry_name_offset:entry_name_end_offset].decode('utf-8')
-        entry_offset_in_data = entry_info[1] - data_offset
-        entry_size = entry_info[2]
-        self.entry_table.append((entry_name, entry_offset_in_data, entry_size))
+  def validate_header(self, buffer):
+    # codice di validazione
+
+   def extract_entries(self, buffer):
+    # codice per estrarre le entry
+
+# Creazione istanza PRIMA dell'uso  
+    unpacker = PupUnpacker()  
+
+# Chiamata metodo
+    unpacker.extract_pup()
 
 def extract_all(self, output_dir):
+
     """Estrae tutti i file nel PUP nella directory di output specificata"""
     # Controlla se la directory di output esiste, altrimenti la crea
     if not os.path.exists(output_dir):
@@ -385,6 +365,7 @@ def extract_all(self, output_dir):
             f.write(self.buffer[entry_offset:entry_offset+entry_size])
 
 def extract_file(self, output_dir, file_name):
+    
     """Estrae un singolo file dal PUP nella directory di output specificata"""
     # Cerca il file nella lista dei file del PUP
 entry = next((e for e in self.entry_table if e[0] == file_name), None)
@@ -395,7 +376,8 @@ if not entry:
 entry_name, entry_offset, entry_size = entry
 with open(os.path.join(output_dir, entry_name), 'wb') as f:
     f.write(self.buffer[entry_offset:entry_offset+entry_size])
-
+    
+   
 class PupUnpacker:
     def __init__(self, master):
         self.master = master
@@ -470,4 +452,4 @@ def select_file(self):
     # Versione del file supportata dalla classe Pup
     VERSION = b'\x00\x00\x00\x01'
    
-   #THE END OF PROGRAM
+   #THE END OF PROGRAM :?)
