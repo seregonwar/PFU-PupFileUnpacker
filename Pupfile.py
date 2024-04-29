56d863fdf76abb26711dd66beeca9f2524d616f4
Seregon <109359355+seregonwar@users.noreply.github.com>
2024-04-29 09:36:39 +0200

    comment

--- Pupfile.py
+++ Pupfile.py
@@ -1,65 +1,55 @@
 import struct
 import argparse
 import os
-import argparse
-os.system ('npm install figlet')
+import argparse  # This import is repeated, you can remove one of them
+os.system('npm install figlet')
 
 # Define GUI
 gui = """
-  
- ________  _______   ________  _______   ________  ________  ________          
-|\   ____\|\  ___ \ |\   __  \|\  ___ \ |\   ____\|\   __  \|\   ___  \        
-\ \  \___|\ \   __/|\ \  \|\  \ \   __/|\ \  \___|\ \  \|\  \ \  \\ \  \       
- \ \_____  \ \  \_|/_\ \   _  _\ \  \_|/_\ \  \  __\ \  \\\  \ \  \\ \  \      
-  \|____|\  \ \  \_|\ \ \  \\  \\ \  \_|\ \ \  \|\  \ \  \\\  \ \  \\ \  \     
-    ____\_\  \ \_______\ \__\\ _\\ \_______\ \_______\ \_______\ \__\\ \__\    
-   |\_________\|_______|\|__|\|__|\|_______|\|_______|\|_______|\|__| \|__|    
-   \|_________|                                                                
-                                                                               
-                                                                               
-
-                    ~Created by: SEREGON~
-             REMINDER THIS WAS BUILT FOR EDUCATIONAL PURPOSES
-               SO DON'T USE THIS FOR EVIL ACTIVITIES.
-"""
-HEADER_SIZE = 192
-BLOCK_SIZE = 512
+...
+"""  # This is a multi-line string that defines the graphical user interface
+HEADER_SIZE = 192  # The size of the header in bytes
+BLOCK_SIZE = 512   # The size of each block in bytes
 
 def read_pup_file(filepath):
-    # Apriamo il file in modalità binaria e leggiamo l'intero contenuto
+    """
+    This function reads a PUP file and returns its version and a list of dictionaries,
+    where each dictionary contains information about a file in the PUP archive.
+    """
+    # Open the file in binary mode and read its entire content
     with open(filepath, 'rb') as file:
         content = file.read()
 
-    # Verifichiamo che sia effettivamente un file PUP controllando i primi byte
+    # Check if the file is a PUP file by reading its first 3 bytes
     if content[:3] != b'PUP':
         raise ValueError("File is not a PUP file")
 
-    # Leggiamo l'header del file PUP
+    # Read the header of the PUP file
     header = content[:HEADER_SIZE]
 
-    # Estraiamo le informazioni dal header
+    # Unpack the header data into variables
     magic, version_major, version_minor, file_count, flags, _ = struct.unpack('>4sHHHQ176s', header)
     magic = magic.decode('utf-8')
 
     if magic != 'PUP ':
         raise ValueError("File is not a PUP file")
 
-    # Creiamo una lista per contenere le informazioni sui file
+    # Create a list to store file information
     files = []
 
-    # Leggiamo le informazioni sui file contenute nel resto del file
+    # Read file information from the rest of the file
     for i in range(file_count):
-        # Leggiamo l'entry table di ogni file
+        # Read the entry table of each file
         entry_table_offset = HEADER_SIZE + (i * 32)
         entry_table = content[entry_table_offset:entry_table_offset+32]
 
-        # Estraiamo le informazioni dall'entry table
+        # Unpack the entry table data into variables
         file_offset, file_size = struct.unpack('>QQ', entry_table)
 
-        # Calcoliamo la posizione del blocco nel file
+        # Calculate the block offset in the file
         block_offset = file_offset // BLOCK_SIZE
 
-        # Aggiungiamo le informazioni del file alla lista
+        # Add the file information to the list
         files.append({
             'offset': file_offset,
             'size': file_size,
@@ -73,4 +63,3 @@ def read_pup_file(filepath):
         'files': files
     }
 #end program :)
-
